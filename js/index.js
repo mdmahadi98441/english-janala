5c6f798d19c5886de353b2b5a92f9fa782474feb
mdmahadi98441-1git config --global user.name mdmahadi98441-1 <mdmahadi98441@gmail.com>
2026-09-14 21:36:40 +0600

    ok

--- js/index.js
+++ js/index.js
@@ -1,9 +1,23 @@
 const loadLesson = ()=> {
     fetch("https://openapi.programming-hero.com/api/levels/all")
     .then(res => res.json())
-    .then(json => console.log(json))
+    .then(json => {
+        console.log(json.data)
+        displayLesson(json.data)
+    })
 }
 const displayLesson = (lessons)=>{
-    
+    const lessonContainer = document.getElementById('lesson-container')
+    lessonContainer.innerHTML = ``
+
+    for(let lesson of lessons){
+        const newDiv = document.createElement('div')
+        newDiv.innerHTML = `
+                
+        <button class="btn btn-outline btn-primary font-bold"><i class="fa-solid fa-book-open"></i> Learn-${lesson.level_no}</button>
+                        
+        `
+        lessonContainer.append(newDiv)
+    }
 }
 loadLesson()
